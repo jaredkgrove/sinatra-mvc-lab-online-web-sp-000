bfc5df0600223d2910df574e8f9118909870e281
Jared Grove <jaredkgrove@gmail.com>
2019-07-08 22:19:47 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,12 +4,10 @@ attr_reader :english_text, :pig_latin_text
     @english_text = english_text
   end
 
-  def pig_latinize
+  def pig_latin_text
     word_array = @english_text.split(" ")
     word_array.each do |word|
-      word.split("").collect do |letter|
-
-      end
+      pig_latinize(word)
     end
   end
 
