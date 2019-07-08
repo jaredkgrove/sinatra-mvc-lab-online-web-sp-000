515d83d238b43628b1a335a31bbf4fa598646a06
Jared Grove <jaredkgrove@gmail.com>
2019-07-08 22:20:20 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,8 +6,16 @@ attr_reader :english_text, :pig_latin_text
 
   def pig_latin_text
     word_array = @english_text.split(" ")
-    word_array.each do |word|
+    word_array.collect do |word|
       pig_latinize(word)
+    end
+  end
+
+  def pig_latinize(word)
+    if starts_with_vowel(word)
+      
+    else
+       
     end
   end
 
