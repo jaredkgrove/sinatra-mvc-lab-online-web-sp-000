492ead5f4cb4eabfa0887d01f397d476f97ac0f8
Jared Grove <jaredkgrove@gmail.com>
2019-07-08 22:51:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
   end
 
   post "/" do
-    @pig_latin = PigLatinizer.new(params[:english_text])
+    @pig_latin = PigLatinizer.new(params[:user_phrase])
     erb :pig_latin
   end
 end
